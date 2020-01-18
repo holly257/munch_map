784d0c978471968880d2b608dcada1ec6e29d578
Holly Rogers <hollyrogers@Hollys-Air.lan>
2020-01-18 10:37:33 -0500

    foratting js code and took off comments

--- script.js
+++ script.js
@@ -2,21 +2,15 @@
 
 const zomatoUrl = 'https://developers.zomato.com/api/v2.1/';
 const zomatoKey = 'dfcbf4ec3afff8937560994206294706';
-
 const mapBoxKey = 'pk.eyJ1IjoiaG9sbHktMjkzODQ3IiwiYSI6ImNrNTlycHgyZjBlc20zb24zZHhvbGpnaGgifQ.4wuSuhP7Za_lKtKMiGx2lg'
 const mapBoxUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'
-
 const states = [ 'AL', 'AK', 'AS', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FM', 'FL', 'GA', 'GU', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MH', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'MP', 'OH', 'OK', 'OR', 'PW', 'PA', 'PR', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VI', 'VA', 'WA', 'WV', 'WI', 'WY' ];
-states.forEach(element =>
-    $('#js-search-state').append(`<option id='${element}'>${element}</option>`)
-)
 
-// variable i had to make to fix a weird issue where the else statement 
-// passed the error message even if the previous statement did what it had to do
 let error = true;
-
 let lat = 0;
 let lon = 0;
+let localMarker = [];
+let newStr = "";
 
 $('#start-app').on('click', function(){
 	$('#first-page').hide();
@@ -34,14 +28,15 @@ $('#new-search-button').on('click', function(){
 	$('#second-page').show();
 })
 
+states.forEach(element =>
+    $('#js-search-state').append(`<option id='${element}'>${element}</option>`)
+)
+
 function formatQueryParams(params) {
   const queryItems = Object.keys(params)
     .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
   return queryItems.join('&');
 }
-
-// this function will display the results showing the list of restaurants and their info
-let localMarker = [];
 
 function displayResults(responseJson) {
 	error = true;
@@ -64,8 +59,6 @@ function displayResults(responseJson) {
 		<br>`
 		);
 
-		// Defining a variable that represents the coordinates and 
-		// type of pointer for each location that is returned.
 		localMarker.push(
 			{ 
 			type: 'Feature',
@@ -81,7 +74,6 @@ function displayResults(responseJson) {
 	showMap();
 }
 
-// this funciton sets the map properties, zoom, and location marker
 function showMap(){
 	mapboxgl.accessToken = 'pk.eyJ1IjoiaG9sbHktMjkzODQ3IiwiYSI6ImNrNTlybDc0YTEydnIzZ3A3bHc5eHZwaWgifQ.7B75rcVKQJASnlD_-yIDkQ';
 	let map = new mapboxgl.Map({
@@ -99,39 +91,33 @@ function showMap(){
 	})
 }
 
-// this function uses the /search parameter with the id of the cuisine(s) and city id, reterns restautant data
-// so we can show them name, url, address, cusine name, rating, phone number, operation hours, menu url
 function getRestaurantList(cityId, cuisineId) {
 	const newUrl = zomatoUrl + 'search?' + `entity_id=${cityId}&entity_type=city&cuisines=${cuisineId}&start=5&count=15`;
 
 	fetch(newUrl, {
-				method: 'get',
-				headers: {
-					'user-key': zomatoKey
-				}
-			})
-			.then(response => {
-		      if (response.ok) {
-		        return response.json();
-		      }
-		      throw new Error(response.statusText);
-		    })
-		    .then(responseJson => displayResults(responseJson))
-		    .catch(err => {
-		    	$("#js-error-message").removeClass("hidden");
-		    	$('#js-error-message').text(`Something went wrong: ${err.message}`);
-		    });
+		method: 'get',
+		headers: {
+			'user-key': zomatoKey
+		}
+	})
+		.then(response => {
+			if (response.ok) {
+			return response.json();
+			}
+			throw new Error(response.statusText);
+		})
+		.then(responseJson => displayResults(responseJson))
+		.catch(err => {
+			$("#js-error-message").removeClass("hidden");
+			$('#js-error-message').text(`Something went wrong: ${err.message}`);
+		});
 }
 
-// this function is going to take the user category cuisine and get its id
-// Im gonna need to compare the category with what responseJson returns and then get the id
-// if they dont provide a category then we can put the value as all, and it will show all of them
 function getCuisineId(responseJson, cityId) {
 	let categoryGiven = $('#js-search-category').val();
 	formatEntry(categoryGiven);
 	categoryGiven = newStr;
-	// if categoryGiven equals to an empty string then make it equal to all
-	// and call getRestaurantList function with the cityid and categoryGiven
+	
 	if(categoryGiven == "" || categoryGiven == "all" || categoryGiven == "All") {
 		categoryGiven = "all";
 		getRestaurantList(cityId, categoryGiven)
@@ -152,7 +138,6 @@ function getCuisineId(responseJson, cityId) {
 	} 
 }
 
-// this functions gets the city id and uses it to get a list of cuisines
 function getCuisineList(responseJson, locationGiven) {
 	for (let i = 0; i < responseJson.location_suggestions.length; i++){
 		if(responseJson.location_suggestions[i].name == locationGiven) {
@@ -185,7 +170,6 @@ function getCuisineList(responseJson, locationGiven) {
 	} 
 }
 
-// this functions checks if the users city is there and gets it's id
 function getCityId(cityGiven, locationGiven) {
 	const params = {
     q: cityGiven,
@@ -215,21 +199,21 @@ function watchForm() {
 	$('#second-page').hide();
 	$('#info').hide();
 
-  $('form').submit(event => {
-    event.preventDefault();
-    const stateGiven = $('#js-search-state').val();
-	let cityGiven = $('#js-search-city').val();
-	formatEntry(cityGiven);
+  	$('form').submit(event => {
+		event.preventDefault();
+		const stateGiven = $('#js-search-state').val();
+		let cityGiven = $('#js-search-city').val();
+		formatEntry(cityGiven);
 
-    $("#js-error-message").empty();
-    $("#js-error-message").addClass("hidden");
-	$("#results-list").empty();
-	$("#results").addClass("hidden");
+		$("#js-error-message").empty();
+		$("#js-error-message").addClass("hidden");
+		$("#results-list").empty();
+		$("#results").addClass("hidden");
 
-	cityGiven = newStr;
-    let locationGiven = `${cityGiven}, ${stateGiven}`;
-		getCityId(cityGiven, locationGiven);
-  });
+		cityGiven = newStr;
+		let locationGiven = `${cityGiven}, ${stateGiven}`;
+			getCityId(cityGiven, locationGiven);
+	});
   
 }
 
@@ -242,6 +226,4 @@ function formatEntry(word){
 	return newStr;
 }
 
-let newStr = "";
-  
 $(watchForm);
